32fa3a53af3036f0813e947b1f3308de964fe20d
Josh C <coop@sitesbycoop.com>
2015-12-08 20:35:37 -0500

    added fucntions.php

---
+++ wp-content/themes/sterlingv3/function.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ *  Author: Josh Cooper 
+ *  URL: sitesbycoop.com
+ *  Custom functions, support, custom post types and more.
+ */
+
+/*------------------------------------*\
+	External Modules/Files
+\*------------------------------------*/
+
+// Load any external files you have here
+
+/*------------------------------------*\
+	Theme Support
+\*------------------------------------*/
+
+if (!isset($content_width))
+{
+    $content_width = 900;
+}
+
+if (function_exists('add_theme_support'))
+{
+    // Add Menu Support
+    add_theme_support('menus');
+
+    // Add Thumbnail Theme Support
+    add_theme_support('post-thumbnails'); 
+    add_image_size('large', 700, '', true); // Large Thumbnail
+    add_image_size('medium', 250, '', true); // Medium Thumbnail
+    add_image_size('small', 120, '', true); // Small Thumbnail
+    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+	add_image_size('bio-pic', 317, 317, true); // 317 Pixels wide, no vertical scale
+
+    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
+    /*add_theme_support('custom-background', array(
+	'default-color' => 'FFF',
+	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
+    ));*/
+
+    // Add Support for Custom Header - Uncomment below if you're going to use
+    /*add_theme_support('custom-header', array(
+	'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
+	'header-text'			=> false,
+	'default-text-color'		=> '000',
+	'width'				=> 1000,
+	'height'			=> 198,
+	'random-default'		=> false,
+	'wp-head-callback'		=> $wphead_cb,
+	'admin-head-callback'		=> $adminhead_cb,
+	'admin-preview-callback'	=> $adminpreview_cb
+    ));*/
+
+    // Enables post and comment RSS feed links to head
+    add_theme_support('automatic-feed-links');
+
+    // Localisation Support
+    load_theme_textdomain('sterlingv3', get_template_directory() . '/languages');
+}
+
+/*------------------------------------*\
+	Functions
+\*------------------------------------*/
+
+// HTML5 Blank navigation
+function sterlingv3_nav()
+{
+	wp_nav_menu(
+	array(
+		'theme_location'  => 'header-menu',
+		'menu'            => '',
+		'container'       => 'div',
+		'container_class' => 'menu-{menu slug}-container',
+		'container_id'    => '',
+		'menu_class'      => 'menu',
+		'menu_id'         => '',
+		'echo'            => true,
+		'fallback_cb'     => 'wp_page_menu',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '<ul id="menu">%3$s</ul>',
+		'depth'           => 0,
+		'walker'          => ''
+		)
+	);
+}
+
+/*------------------------------------*\
+	Functions
+\*------------------------------------*/
+
+
+// Load Stylesheet
+function sterlingv3_styles()
+{
+
+    wp_register_style('sterlingv3', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('sterlingv3'); // Enqueue it!
+
+}
